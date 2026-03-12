dd8a12eb7d14fe24a007b42993808dae7e632159
sathvik7013 <sathvik7013@github.com>
2026-03-12 14:06:12 +0530

    Fix Maven wrapper: add JDK path and multiModuleProjectDirectory property

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,4 +1,5 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Microsoft\jdk-21.0.8.9-hotspot
 set MAVEN_PROJECTBASEDIR=%~dp0
-"%JAVA_HOME%\bin\java" -classpath "%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar" org.apache.maven.wrapper.MavenWrapperMain %*
+if "%MAVEN_PROJECTBASEDIR:~-1%"=="\" set MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%
+"%JAVA_HOME%\bin\java" -Dmaven.multiModuleProjectDirectory="%MAVEN_PROJECTBASEDIR%" -classpath "%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar" org.apache.maven.wrapper.MavenWrapperMain %*
